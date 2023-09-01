2deb459829bfb56df43e007a8f90e3f7ee765c82
LukaszJ <jozwiak.lukasz6@gmail.com>
2023-09-01 22:54:15 +0200

    Added route and controller

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -11,7 +11,7 @@ class IndexController extends Controller
      */
     public function index()
     {
-        return inertia('Index/Index');
+        return inertia('Index/Index', ['message' => 'Message from world']);
     }
 
     /**
--- resources/js/Pages/Index/Index.vue
+++ resources/js/Pages/Index/Index.vue
@@ -1,11 +1,15 @@
 <script setup>
   import {Link} from '@inertiajs/vue3'
+  defineProps({
+    message: String
+  })
 
 </script>
 
 <template>
 <div>Index</div>
   <Link href="/hello">Show Page</Link>
+  To jest: {{ message }}
 </template>
 
 <style scoped>

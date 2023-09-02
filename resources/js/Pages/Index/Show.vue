4aaa8d9549bc0387f43496a4b189b03addd0aaf2
LukaszJ <jozwiak.lukasz6@gmail.com>
2023-09-02 21:19:30 +0200

    added layout, config

--- resources/js/Pages/Index/Show.vue
+++ resources/js/Pages/Index/Show.vue
@@ -1,11 +1,11 @@
-<script setup>
-import {Link} from '@inertiajs/vue3'
-</script>
-
 <template>
   <div>Show</div>
   <Link href="/">Main Page</Link>
 </template>
+
+<script setup>
+import {Link} from '@inertiajs/vue3'
+</script>
 
 <style scoped>
 
